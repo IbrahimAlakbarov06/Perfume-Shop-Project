420643088030482ec440e88e0f7686a370711d86
IbrahimAlakbarov06 <ibrahim.alakbarov06@gmail.com>
2025-03-26 17:23:17 +0400

    perfume-shop

---
+++ src/main/java/org/perfume/perfumebackend/dto/ProductDto.java
@@ -0,0 +1,20 @@
+package org.perfume.perfumebackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String brandName;
+    private String categoryName;
+    private Integer stockQuantity;
+    private String imageUrl;
+}
