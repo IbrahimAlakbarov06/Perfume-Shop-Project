82a1305d81baa9e09018dbc558b1606737731f94
IbrahimAlakbarov06 <ibrahim.alakbarov2006@gmail.com>
2025-05-27 15:10:02 +0400

    Update OrderItemRequest with missing import

---
+++ src/main/java/org/perfume/model/dto/request/PasswordUpdateRequest.java
@@ -0,0 +1,20 @@
+package org.perfume.model.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordUpdateRequest {
+    @NotBlank(message = "Current password is required")
+    private String currentPassword;
+
+    @NotBlank(message = "New password is required")
+    private String newPassword;
+
+    @NotBlank(message = "Password confirmation is required")
+    private String confirmPassword;
+}
